2a2f592ed69785e590ea1c6119ec898c21849ca0
Saint-Jean Germila <germila@noukod.com>
2019-03-06 19:02:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function driversWithRevenueOver(drivers, revenue){
   }
 
   function driverNamesWithRevenueOver (drivers, revenue) {
-    return driverNamesWithRevenueOver (drivers, revenue)
+    return driversWithRevenueOver (drivers, revenue)
     .map(function (driver) {
       return driver.name;
     });
